9de9d4b81c926afb70775711f0c45a4093719e88
Brandy Foster <brandy.foster@detroitlabs.com>
2014-09-16 21:39:04 -0400

    started book demo. added scanner

---
+++ src/com/sample/bookassignment/BookDemo.java
@@ -0,0 +1,11 @@
+package com.sample.bookassignment;
+
+/**
+ * Created by BFineRocks on 9/16/14.
+ */
+import java.util.Scanner;
+public class BookDemo {
+    public static void main(String [] args){
+        Scanner userInput = new Scanner(System.in);
+    }
+}
